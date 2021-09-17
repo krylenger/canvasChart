c5898e0a1f8752ebf8878b53c47a66b24da85368
Yevhen Krylovskyi <krylenger@gmail.com>
2021-09-17 12:04:16 +0300

    first dashed line

--- chart.js
+++ chart.js
@@ -10,19 +10,43 @@ const draw = () => {
 	if (canvas.getContext) {
 		var ctx = canvas.getContext("2d");
 
+		// Y vector
+		ctx.lineWidth = 3;
 		ctx.beginPath();
-		ctx.moveTo(75, 50);
-		ctx.lineTo(100, 75);
-		ctx.lineTo(100, 25);
-		ctx.closePath()
+		ctx.moveTo(56.5, 296);
+		ctx.lineTo(56.5, 32);
 		ctx.stroke();
 
+		//X vector
 		ctx.beginPath();
-		ctx.moveTo(75, 50);
-		ctx.lineTo(10, 75);
-		ctx.lineTo(10, 25);
+		ctx.moveTo(56.5, 295.5);
+		ctx.lineTo(462, 295.5);
+		ctx.stroke();
 
-		ctx.fill();
+		const triangleY = new Path2D();
+		triangleY.moveTo(56.5, 29);
+		triangleY.lineTo(48, 44);
+		triangleY.lineTo(65, 44);
+		ctx.fill(triangleY)
+
+		const triangleX = new Path2D();
+		triangleX.moveTo(465, 295.5);
+		triangleX.lineTo(450, 286.5);
+		triangleX.lineTo(450, 303.5);
+		ctx.fill(triangleX)
+
+
+		//dashed line
+		ctx.lineWidth = 2;
+		ctx.setLineDash([5, 5]);
+		ctx.beginPath();
+		ctx.moveTo(57.5, 256);
+		ctx.lineTo(448.5, 256);
+		// ctx.lineTo(450, 303.5);
+		ctx.stroke();
+
+
+
 	}
 }
 
